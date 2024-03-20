f9e0a147eda82263a4ceb28c5201b3b8f0acd92a
Nicholas Rodrigues <nicholasgabriel65@gmail.com>
2024-03-20 11:45:36 -0300

    New: Counting access on the app endpoint

--- main.go
+++ main.go
@@ -5,6 +5,27 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = 0
+}
+
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -26,8 +47,12 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 func createServer() *http.Server {
 	const port = "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.StripPrefix("/app", http.FileServer(http.Dir("./app/"))))
+	apiConfig := apiConfig{}
+
+	mux.Handle("/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("./app/")))))
 	mux.HandleFunc("/healthz", readinessHandler)
+	mux.Handle("/metrics", http.HandlerFunc(apiConfig.metricsHandler))
+	mux.Handle("/reset", http.HandlerFunc(apiConfig.resetHandler))
 	corsMux := middlewareCors(mux)
 
 	server := &http.Server{
